c967a178e7e565fac0cd2ee4080cfc625553805f
22730067 <22730067@ms.uit.edu.vn>
2025-08-24 12:41:52 +0700

    create class board

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include <cstdlib>
 #include <conio.h>
 #include <chrono>
+#include <sstream>
 // ----------------------------------------------------------------------------
 using namespace std;
 // ----------------------------------------------------------------------------
-const int ROW = 23;
-const int COL = 80;
-bool active_board[ROW][COL];
 int game_state = 0;
 // ----------------------------------------------------------------------------
 struct Point
 {
     int x,y;
 };
+struct RGB
+{
+	int r,g,b;
+};
+const int ROW = 42;
+const int COL = 80;
 // ----------------------------------------------------------------------------
 int get_input();
 void enable_special_character();
@@ -22,45 +26,101 @@ void gotoxy( int column, int line );
 void show_game();
 void show_menu();
 // ----------------------------------------------------------------------------
+class Board
+{
+public:
+	const std::string item = "▄";	// y0 = bg, y1 = fg
+	RGB board[ROW][COL];
+	bool active_board[ROW][COL];
+
+	Board()
+	{
+		for (int y = 0; y < ROW; y++) {
+			for (int x = 0; x < COL; x++) {
+				board[y][x].r = 255;
+				board[y][x].g = 255;
+				board[y][x].b = 255;
+				active_board[y][x] = false;
+			}
+		}
+	}
+	void update_board(const Point src, int type)
+	{
+		RGB color;
+		switch (type)
+		{
+			case 0:
+				color.r = 0;
+				color.g = 0;
+				color.b = 0;
+				break;
+			case 1:
+				color.r = 255;
+				color.g = 255;
+				color.b = 255;
+				break;
+			default: break;
+		}
+		
+		board[src.y][src.x].r = color.r;
+		board[src.y][src.x].g = color.g;
+		board[src.y][src.x].b = color.b;
+	}
+	void render()
+	{
+		std::stringstream frameBuffer;
+		frameBuffer << "\033[H";
+		for (int row = 0; row < ROW; row+=2) {
+			for (int col = 0; col < COL; col++) {
+				frameBuffer << "\033[48;2;" << std::to_string(board[row][col].r) << ";" << std::to_string(board[row][col].g) << ";" << std::to_string(board[row][col].b) << "m" << "\033[38;2;" << std::to_string(board[row+1][col].r) << ";" << std::to_string(board[row+1][col].g) << ";" << std::to_string(board[row+1][col].b) << "m" << item; 
+			}
+			frameBuffer << "\033[0m\n";
+		}
+		std::cout << frameBuffer.str();
+		std::cout.flush();
+	}
+};
 class CONRAN
 {
 public:
     struct Point A[100];
+	Board game_board;
 	Point old;
     int DoDai;
 	
     CONRAN()
 	{
         DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
+        A[0].x = 12; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        A[2].x = 10; A[2].y = 10;
+		old.x = -1;
+		old.y = -1;
     }
     void Ve()
 	{
         for (int i = 0; i < DoDai; i++)
 		{
-			if (active_board[A[i].y][A[i].x]) continue;
-            gotoxy(A[i].x,A[i].y);
-            cout<<"▄";
-			active_board[A[i].y][A[i].x] = true;
+			if (game_board.active_board[A[i].y][A[i].x]) continue;
+			game_board.update_board(A[i],0);
+			game_board.active_board[A[i].y][A[i].x] = true;
         }
+		game_board.render();
     }
 	void Update()
 	{
-		if (!active_board[A[0].y][A[0].x]) 
+		if (game_board.active_board[A[0].y][A[0].x] == false) 
 		{
-			gotoxy(A[0].x,A[0].y);
-			cout<<"▄";
-			active_board[A[0].y][A[0].x] = true;
+			game_board.update_board(A[0],0);
+			game_board.active_board[A[0].y][A[0].x] = true;
 		}
 		
-		if (active_board[old.y][old.x]) 
+		if (game_board.active_board[old.y][old.x]) 
 		{
-			gotoxy(old.x,old.y);
-			cout<<" ";
-			active_board[old.y][old.x] = false;
+			game_board.update_board(old,1);
+			game_board.active_board[old.y][old.x] = false;
 		}
+		game_board.render();
 	}
     void DiChuyen(int Huong)
 	{
@@ -93,14 +153,10 @@ public:
 // ----------------------------------------------------------------------------
 int main()
 {
+	
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(nullptr);
 	enable_special_character();
-	for (int i = 0; i < ROW; ++i)
-	{
-		for (int j = 0; j < COL; ++j)
-		{
-			active_board[i][j] = false;
-		}
-	}
     show_menu();
     return 0;
 }
@@ -129,7 +185,8 @@ void show_menu()
 	std::cout << "write your name: ";
 	std::string name;
 	std::cin >> name;
-	std::cout << "your name: " << name << '\n';
+	std::cout << "your name: " << name;
+	std::cout.flush();
 	if (name == "abc")
 	{
 		Sleep(2000);
@@ -141,6 +198,7 @@ void show_menu()
 
 void show_game()
 {
+	std::cout << "\033[?25l";
 	CONRAN r;
     int Huong = 0;
     char t;
