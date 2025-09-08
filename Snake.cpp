170ca510b8aa080fc00853f5c0bad552d37dc958
uit-25730015-hieucanh <25730015@ms.uit.edu.vn>
2025-09-08 21:02:03 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,14 +1,14 @@
 #include "Snake.h"
 
 /*
- * hàm di chuyển rắn
+ * Hàm thực hiện việc di chuyển rắn trên bản đồ
  *
  */
 void Snake::move()
 {
-	// cập nhật hướng di chuyển
+	// Cập nhật hướng di chuyển
 	direction = nextDirection;
-	// tính toán vị trí đầu mới
+	// Tính toán vị trí đầu mới
 	Point newHead = body.front();
 	switch (direction)
 	{
@@ -18,9 +18,9 @@ void Snake::move()
 	case Direction::RIGHT: newHead.x++; break;
 	case Direction::NONE:  break;
 	}
-	// thêm đầu mới vào thân rắn
+	// Thêm đầu mới vào thân rắn
 	body.push_front(newHead);
-	// nếu rắn đói (point=0) thì xóa đuôi rắn và đặt isMove=true để cho phép di chuyển tiếp
+	// Nếu rắn đói (point=0) thì xóa đuôi rắn và đặt isMove=true để cho phép di chuyển tiếp
 	if (point > 0) point--;
 	else
 	{
@@ -30,17 +30,17 @@ void Snake::move()
 }
 
 /*
- * hàm kiểm tra rắn có tự cắn hay không
+ * Hàm kiểm tra rắn có tự cắn hay không
  */
 bool Snake::checkSelfCollision() const
 {
 	if (body.size() < 4) return false;	// rắn 3 khúc không thể tự cắn được 
-	// lấy vị trí đầu rắn 
+	// Lấy vị trí đầu rắn 
 	Point head = body.front();
-	// nếu đầu rắn vượt quá phạm vi chơi thì trả về va chạm 
+	// Nếu đầu rắn vượt quá phạm vi chơi thì trả về va chạm 
 	if (head.x < 0 || head.y < 0) return true;
 	if (head.x >= BOARD_WIDTH || head.y >= BOARD_HEIGHT) return true;
-	// nếu đầu rắn trùng với thân rắn thì tự cắn 
+	// Nếu đầu rắn trùng với thân rắn thì tự cắn 
 	for (size_t i = 1; i < body.size(); ++i)
 	{
 		if (head == body[i])
